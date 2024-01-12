f763c473c824fca44dfd878b379f1c413f82cc65
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-12 18:40:19 -0500

    Update My App.swift

--- My App/My App.swift
+++ My App/My App.swift
@@ -1,6 +1,21 @@
 import SwiftUI
 
 @main
+struct Main {
+    static func main() {
+        let args = CommandLine.arguments
+        if args.count > 1 {
+            if args[1] == "install" {
+                
+            } else if args[1] == "uninstall" {
+                
+            } 
+        } else {
+            MyApp.main()
+        }
+    }
+}
+
 struct MyApp: App {
     var body: some Scene {
         WindowGroup {
