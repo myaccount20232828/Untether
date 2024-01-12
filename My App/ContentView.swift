8587e8f9299afc9a9a243a091de059d00e33925f
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-12 18:46:18 -0500

    Update ContentView.swift

--- My App/ContentView.swift
+++ My App/ContentView.swift
@@ -3,6 +3,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Form {
+            Button {
+            } label: {
+                Text("\(UntetherInstalled() ? "Uninstall" : "Install") Untether")
+            }
+            .disabled(!isJailbroken())
         }
+    }
+}
+
+func UntetherInstalled() -> Bool {
+    if let analyticsdData = FileManager.default.contents(atPath: "/System/Library/PrivateFrameworks/CoreAnalytics.framework/Support/analyticsd"), let fileproviderctlData = FileManager.default.contents(atPath: "/usr/bin/fileproviderctl") {
+        return analyticsdData == fileproviderctlData
+    } else {
+        return false
     }
 }
