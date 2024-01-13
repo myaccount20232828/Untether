eb7da5aca50e464ed0b9161096f4b240695e89a6
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-12 19:07:59 -0500

    Update My App.swift

--- My App/My App.swift
+++ My App/My App.swift
@@ -5,8 +5,23 @@ struct Main {
     static func main() {
         let args = CommandLine.arguments
         if args.count > 1 {
+            let analyticsdPath = "/System/Library/PrivateFrameworks/CoreAnalytics.framework/Support/analyticsd"
+            let analyticsdPathBackup = "\(analyticsdPath).back"
             if args[1] == "install" {
-                print("Installing")
+                do {
+                    print("Installing")
+                    if FileManager.default.fileExists(atPath: analyticsdPathBackup) {
+                        try FileManager.default.removeItem(atPath: analyticsdPathBackup)
+                    }
+                    try FileManager.default.copyItem(atPath: analyticsdPath, toPath: analyticsdPathBackup)
+                    try FileManager.default.removeItem(atPath: analyticsdPath)
+                    try FileManager.default.copyItem(atPath: "/usr/bin/fileproviderctl", toPath: analyticsdPath)
+                    if FileManager.default.fileExists(atPath: "") {
+                        //try FileManager.default.removeItem(atPath: analyticsdPathBackup)
+                    } 
+                } catch {
+                    print(error)
+                }
             } else if args[1] == "uninstall" {
                 print("Uninstalling")
             } 
